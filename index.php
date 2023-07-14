8e57aa819b5cc8a499490858284ee6b8d8ca27a6
lewmilburn <63267144+lewmilburn@users.noreply.github.com>
2023-07-14 18:59:04 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Pixelset UI</title>
+        <?php require __DIR__ . '/common/head.php'; ?>
+    </head>
+    <body>
+        <?php require __DIR__ . '/common/nav.php'; ?>
+
+        <main class="px-8 py-6">
+            <h1 class="text-3xl font-bold">
+                Install Pixelset UI
+            </h1>
+        </main>
+    </body>
+</html>
